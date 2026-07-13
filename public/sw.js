71ff682c4b4de96cb2e020f9ad6b8ef920bf7bd8
Noah Maxwell <noah.maxwell@gmail.com>
2026-07-12 17:58:27 -0700

    fix: bump service worker cache version to v2 and clean up old caches

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lg-remote-v1';
+const CACHE_NAME = 'lg-remote-v2';
 const ASSETS = [
     '/',
     '/index.html',
@@ -8,9 +8,27 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
         .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        Promise.all([
+            self.clients.claim(),
+            caches.keys().then(keys => {
+                return Promise.all(
+                    keys.map(key => {
+                        if (key !== CACHE_NAME) {
+                            return caches.delete(key);
+                        }
+                    })
+                );
+            })
+        ])
     );
 });
 
